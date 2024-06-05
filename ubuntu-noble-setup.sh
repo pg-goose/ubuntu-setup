0026d20490cd62c930338e5949136c2cb5db4a46
pg-goose <pau.galopa.dev@gmail.com>
2024-06-05 21:30:35 +0200

    added packages

--- ubuntu-noble-setup.sh
+++ ubuntu-noble-setup.sh
@@ -1,14 +1,27 @@
+echo "Creando directorio development"
+mkdir -d ~/development
+
+echo "Instalando y configurando essenciales"
 sudo apt update
 sudo apt install wget gpg ca-certificates curl apt-transport-https
 sudo install -m 0755 -d /etc/apt/keyrings
 
+# GH
+echo "Configurando Repo para Github CLI"
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y))
+wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 # VScode
+echo "Configurando Repo para VScode"
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
 
 # Add Docker's official GPG key:
+echo "Configurando Repo para Docker"
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
@@ -18,8 +31,10 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Mozilla Firefox repo
+echo "Configurando Repo para Firefox"
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
 echo '
 Package: *
 Pin: origin packages.mozilla.org
@@ -27,14 +42,52 @@ Pin-Priority: 1000
 ' | sudo tee /etc/apt/preferences.d/mozilla 
 
 # Warp client
+echo "Configurando Repo para cloudflare warp"
 curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 
 # Cloudflared
+echo "Configurando Repo para cloudflare cli"
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
 echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
 
+# Whishlist, gum
+echo "Configurando Repo para charm.sh"
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+
 # Install
+echo "Actualizando lista de packages"
 sudo apt update
-sudo apt install firefox code docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin cloudflare-warp cloudflared
-sudo snap install discord
+
+echo "Purgando firefox snap"
+sudo snap remove firefox
+sudo apt purge firefox
+
+echo "Instalando packages"
+sudo apt install \
+pigz \
+gh  \
+firefox \
+code \
+docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
+python-is-python3 \
+python3.12-venv \
+go-golang \
+cloudflared \
+cloudflare-warp \
+wishlist \
+gum 
+
+# Snaps (ugh)
+sudo snap install discord \
+whatsdesk \
+slack
+
+# Auth github
+gh auth login -w
+
+# My tools
+wget https://github.com/pg-goose/togo/releases/download/pre1.0.0/togo-linux-pre1.0.0
+sudo chmod +x togo-linux-pre1.0.0
+sudo mv togo-linux-pre1.0.0 /usr/bin/togo
